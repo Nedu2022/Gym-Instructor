3fbe468cee444ba71d7fb7c67c18c8ef1b893a2c
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-01-16 21:50:28 +0100

    Adding lost files from backup

--- src/utility/FetchData.js
+++ src/utility/FetchData.js
@@ -1,8 +1,17 @@
-export const exerciseOptions = {
+// export const exerciseOptions = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+//       'X-RapidAPI-Key': '3f6e155f0amsh16c2b2f69ed2054p155891jsn8a25ec5d158b'
+//     }
+//   };
+
+ export const exerciseOptions = {
     method: 'GET',
+    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': '3f6e155f0amsh16c2b2f69ed2054p155891jsn8a25ec5d158b'
+      'X-RapidAPI-Key': '3f6e155f0amsh16c2b2f69ed2054p155891jsn8a25ec5d158b',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
 
